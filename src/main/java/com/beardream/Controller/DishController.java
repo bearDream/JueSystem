72129fa7df4aee44d7525fc2a6a5182173de1f83
soft01 <1833270267@qq.com>
2017-05-09 17:07:02 +0800

    分页

--- src/main/java/com/beardream/Controller/DishController.java
+++ src/main/java/com/beardream/Controller/DishController.java
@@ -1,20 +1,23 @@
 package com.beardream.Controller;
 
 import com.beardream.Utils.ResultUtil;
+import com.beardream.Utils.TextUtil;
 import com.beardream.dao.DishMapper;
-import com.beardream.ioc.PermissionMethod;
-import com.beardream.ioc.PermissionModule;
-import com.beardream.model.Dish;
-import com.beardream.model.DishBusiness;
-import com.beardream.model.Result;
-import com.beardream.model.User;
+import com.beardream.ioc.*;
+import com.beardream.model.*;
 import com.beardream.service.DishService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by laxzh on 2017/5/6.
@@ -62,5 +65,26 @@ public class DishController {
     public Result put(Dish dish){
         System.out.println(dish.getDishId());
         return ResultUtil.success(mDishService.put(dish));
+    }
+
+    @ApiOperation("分页获取角色")
+    @GetMapping("/getpage")
+    @com.beardream.ioc.Log
+    public Result getPage(Role role, @RequestParam(value = "pageNum", required = false)  int pageNum, @RequestParam(value = "pageSize", required = false)  int pageSize, BindingResult bindingResult){
+//        System.out.println(role.getRoleId());
+        System.out.println(pageNum);
+        System.out.println(pageSize);
+        if (!TextUtil.isEmpty(pageNum) || !TextUtil.isEmpty(pageSize)){
+            return ResultUtil.error(-1,"pageNum,pageNum不能为空！");
+        }
+
+        //获取第1页，10条内容，默认查询总数count
+        PageHelper.startPage(pageNum , pageSize).setOrderBy("add_time asc");
+        List<Dish> dishs =dishMapper.findBySelective(new Dish());
+        PageInfo page = new PageInfo(dishs);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("page",page);
+        map.put("list",dishs);
+        return ResultUtil.success(map);
     }
 }
--- src/main/java/com/beardream/Controller/NutritionController.java
+++ src/main/java/com/beardream/Controller/NutritionController.java
@@ -1,22 +1,21 @@
 package com.beardream.Controller;
 
-import antlr.collections.List;
 import com.beardream.Utils.ResultUtil;
-import com.beardream.dao.NumberMapper;
+import com.beardream.Utils.TextUtil;
 import com.beardream.dao.NutritionMapper;
-import com.beardream.dao.UserMapper;
-import com.beardream.model.Nutrition;
-import com.beardream.model.Result;
-import com.beardream.model.User;
+import com.beardream.model.*;
 import com.beardream.service.NutritionService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by laxzh on 2017/5/6.
@@ -60,5 +59,26 @@ public class NutritionController {
     public Result put(Nutrition nutrition) {
         System.out.println(nutrition.getNurtritionId());
         return ResultUtil.success(mNutritionService.put(nutrition));
+    }
+
+    @ApiOperation("分页获取角色")
+    @GetMapping("/getpage")
+    @com.beardream.ioc.Log
+    public Result getPage(Role role, @RequestParam(value = "pageNum", required = false)  int pageNum, @RequestParam(value = "pageSize", required = false)  int pageSize, BindingResult bindingResult){
+//        System.out.println(role.getRoleId());
+        System.out.println(pageNum);
+        System.out.println(pageSize);
+        if (!TextUtil.isEmpty(pageNum) || !TextUtil.isEmpty(pageSize)){
+            return ResultUtil.error(-1,"pageNum,pageNum不能为空！");
+        }
+
+        //获取第1页，10条内容，默认查询总数count
+        PageHelper.startPage(pageNum , pageSize).setOrderBy("add_time asc");
+        List<Nutrition> nutritions =nutritionMapper.findBySelective(new Nutrition());
+        PageInfo page = new PageInfo(nutritions);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("page",page);
+        map.put("list",nutritions);
+        return ResultUtil.success(map);
     }
 }
