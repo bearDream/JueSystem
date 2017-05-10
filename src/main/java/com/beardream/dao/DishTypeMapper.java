c8140fb091155ee06ef6a5342abda892077e0f0a
soft01 <a532281588@qq.com>
2017-05-10 11:12:15 +0800

    菜品分类 增删查改

--- src/main/java/com/beardream/dao/DishTypeMapper.java
+++ src/main/java/com/beardream/dao/DishTypeMapper.java
@@ -1,6 +1,15 @@
 package com.beardream.dao;
 
+import com.beardream.model.Business;
 import com.beardream.model.DishType;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
 
 public interface DishTypeMapper {
     int deleteByPrimaryKey(Integer dishtypeId);
@@ -10,6 +19,9 @@ public interface DishTypeMapper {
     int insertSelective(DishType record);
 
     DishType selectByPrimaryKey(Integer dishtypeId);
+
+    List<DishType> findBySelective(DishType dishType);
+
 
     int updateByPrimaryKeySelective(DishType record);
 
