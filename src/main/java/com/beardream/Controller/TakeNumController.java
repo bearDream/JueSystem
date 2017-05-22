72de3712be5c55993cb9b2d08b0445efcf00d985
bearDream <450848477@qq.com>
2017-05-22 23:37:56 +0800

    后台获取开启排队的商家以及获取在某商家取号（未过期）的所有用户

--- src/main/java/com/beardream/Controller/TakeNumController.java
+++ src/main/java/com/beardream/Controller/TakeNumController.java
@@ -2,39 +2,57 @@ package com.beardream.Controller;
 
 import com.beardream.Utils.ResultUtil;
 import com.beardream.Utils.TextUtil;
+import com.beardream.dao.BusinessMapper;
+import com.beardream.model.Business;
 import com.beardream.model.Number;
 import com.beardream.model.Result;
 import com.beardream.service.TakeNumService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by laxzh on 2017/5/6.
  * 取号控制器
  */
 @RestController
-@RequestMapping("/api/tag")
+@RequestMapping("/api/take")
 @Api(value = "取号服务",description = "提供RESTful风格API的取号的操作")
 public class TakeNumController {
 
     @Autowired
     private TakeNumService mTakeNumService;
 
+    // 获取所有开启排队的商家列表
     @GetMapping
-    public Result takeNum(Number number){
-        if (!TextUtil.isEmpty(number.getBusinessId()) && !TextUtil.isEmpty(number.getPeopleNum())){
-            return ResultUtil.error(-1,"请选择商家并填写人数后又提交");
+    public Result takeNum(@RequestParam(value = "pageNum", defaultValue = "1",required = false)  int pageNum,
+                          @RequestParam(value = "pageSize", defaultValue = "10",required = false)  int pageSize){
+
+        Business business = new Business();
+        // 查找的是开启排号的商家
+        business.setIsTake((byte) 0);
+        Map<String, Object> enableTakeList = mTakeNumService.getEnableTakeBusiness(business, pageNum, pageSize);
+
+        return ResultUtil.success(enableTakeList);
+    }
+
+    // 获取商家的所有排号(注意这里返回的不是分页数据，只是一个list数据)
+    @GetMapping(value = "/getBusinessNum", params = {"businessId"})
+    public Result getNum(@RequestParam("businessId") Integer businessId){
+
+        if (!TextUtil.isEmpty(businessId)) {
+            return ResultUtil.error(-1,"商家id不能为空");
         }
-        number.setIsExpired((byte) 1); // 设置为没有过期
-        Result num = mTakeNumService.takeNum(number);
-        if (num.getCode() != -1)
-            return ResultUtil.success(num.getData());
-        else
-            return ResultUtil.error(-1,num.getMsg());
+
+        Result numList = mTakeNumService.takeNum(businessId);
+        if (numList.getCode() != -1){
+            return ResultUtil.success(numList.getData());
+        }
+        return ResultUtil.error(-1,numList.getMsg());
+
     }
 }
