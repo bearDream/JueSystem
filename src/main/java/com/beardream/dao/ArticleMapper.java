6053942ff6d88cdb7be9157c17f6ca26e722498b
soft01 <a532281588@qq.com>
2017-05-22 11:31:02 +0800

    Controller中添加了商家标签、菜品标签控制器 dao中添加了商家标签、菜品标签Mapper mapper中添加了商家标签、菜品标签的增删查改 model中添加了商家标签和菜品标签 service中添加了商家标签和菜品标签服务

--- src/main/java/com/beardream/dao/ArticleMapper.java
+++ src/main/java/com/beardream/dao/ArticleMapper.java
@@ -1,15 +1,23 @@
 package com.beardream.dao;
 
 import com.beardream.model.Article;
+import com.beardream.model.Business;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
+@Mapper
 public interface ArticleMapper {
-    int deleteByPrimaryKey(Integer articalId);
+    int deleteByPrimaryKey(Integer articleId);
 
     int insert(Article record);
 
     int insertSelective(Article record);
 
-    Article selectByPrimaryKey(Integer articalId);
+    Article selectByPrimaryKey(Integer articleId);
+
+    List<Article> findBySelective(Article article);
 
     int updateByPrimaryKeySelective(Article record);
 
